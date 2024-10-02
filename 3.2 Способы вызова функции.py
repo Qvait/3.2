dfbeac80ea926f94f24506346a26084eaffdb79a
Qvait <brylov.1996@mail.ru>
2024-10-02 14:54:09 +0500

    Add files via upload

---
+++ 3.2 Способы вызова функции.py
@@ -0,0 +1,36 @@
+def send_email (message, recipient, sender = "university.help@gmail.com"):
+    a = "@"
+    b = ".com"
+    c = ".net"
+    d = ".ru"
+    if a in recipient or b in recipient or c in recipient or d in recipient:
+        if sender != recipient:
+            if sender == "university.help@gmail.com":
+                print("Письмо успешно отправлено с адреса ", sender, "на адрес ", recipient)
+            else:
+                print( "НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ!")
+        else:
+            print("Нельзя отправить письмо самому себе!")
+    else:
+        print("Невозможно отправить письмо с адреса ", sender, "на адрес ", recipient)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru',
+    sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
